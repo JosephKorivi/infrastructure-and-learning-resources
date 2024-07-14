57ddefdec32abf9bfacd1f6d978f580f2cd122b0
JosephKorivi <joseph.korivi0911@gmail.com>
2024-07-14 17:36:44 +0530

    next commit

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
         document.getElementById("preloader").style.display = "none";
         document.getElementById("content").style.display = "block";
-    }, 5000); // 5000 milliseconds = 5 seconds
+    }, 1000); // 5000 milliseconds = 5 seconds
 });
