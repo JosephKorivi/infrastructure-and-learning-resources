9eb3105501fd421f115d23469b199c474c5a411e
JosephKorivi <joseph.korivi0911@gmail.com>
2024-07-14 18:03:44 +0530

    updated

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
         document.getElementById("preloader").style.display = "none";
         document.getElementById("content").style.display = "block";
-    }, 1000); // 5000 milliseconds = 5 seconds
+    }, 500); // 5000 milliseconds = 5 seconds
 });
